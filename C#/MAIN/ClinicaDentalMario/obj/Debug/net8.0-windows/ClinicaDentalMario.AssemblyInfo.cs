a3356953388a30ab54d0fc41b4501c075844e0f2
Gust4av033 <bryangustabo@gmail.com>
2026-08-31 21:22:42 -0600

    Algunos cambios para el modulo de pacientes, si estos sirven se podria decir que el modulo de pacientes estaria al 100%

--- C#/MAIN/ClinicaDentalMario/obj/Debug/net8.0-windows/ClinicaDentalMario.AssemblyInfo.cs
+++ C#/MAIN/ClinicaDentalMario/obj/Debug/net8.0-windows/ClinicaDentalMario.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinicaDentalMario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c65e24acabd0238505d28423f2e4d24c3a0c5a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5e727d646ad9e925cfb32dc4bd1dd76682e0045")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinicaDentalMario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicaDentalMario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

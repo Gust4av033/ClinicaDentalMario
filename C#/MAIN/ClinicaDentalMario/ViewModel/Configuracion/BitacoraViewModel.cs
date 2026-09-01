df60fc9784498574cacb6257ce6c046050a872d7
Gust4av033 <137726376+Gust4av033@users.noreply.github.com>
2026-08-31 21:09:48 -0600

    refactor: use audit repository from view model

--- C#/MAIN/ClinicaDentalMario/ViewModel/Configuracion/BitacoraViewModel.cs
+++ C#/MAIN/ClinicaDentalMario/ViewModel/Configuracion/BitacoraViewModel.cs
@@ -1,11 +1,9 @@
 using ClinicaDentalMario.Common;
-using ClinicaDentalMario.Data;
 using ClinicaDentalMario.Models;
+using ClinicaDentalMario.Repositories;
 using ClinicaDentalMario.Services;
 using ClinicaDentalMario.ViewModel.Base;
-using Dapper;
 using System.Collections.ObjectModel;
-using System.Data;
 using System.Windows;
 using System.Windows.Input;
 
@@ -14,6 +12,7 @@ namespace ClinicaDentalMario.ViewModel.Configuracion
     public class BitacoraViewModel : ViewModelBase
     {
         private readonly IPermissionService _permissionService;
+        private readonly BitacoraRepository _bitacoraRepository;
 
         private ObservableCollection<BitacoraModel> _registrosAuditoria = new();
         public ObservableCollection<BitacoraModel> RegistrosAuditoria
@@ -36,14 +35,15 @@ namespace ClinicaDentalMario.ViewModel.Configuracion
         {
             Titulo = "Auditoría y Bitácora del Sistema";
             _permissionService = new PermissionService();
+            _bitacoraRepository = new BitacoraRepository();
 
             if (!_permissionService.TienePermiso(PermisoSistema.VerBitacora))
             {
                 throw new UnauthorizedAccessException("El usuario actual no puede consultar la bitácora.");
             }
 
-            CargarBitacoraCommand = new RelayCommand(async p => await CargarAsync());
-            BuscarCommand = new RelayCommand(async p => await CargarAsync());
+            CargarBitacoraCommand = new RelayCommand(async _ => await CargarAsync());
+            BuscarCommand = new RelayCommand(async _ => await CargarAsync());
 
             _ = CargarAsync();
         }
@@ -61,23 +61,7 @@ namespace ClinicaDentalMario.ViewModel.Configuracion
             EstaCargando = true;
             try
             {
-                using IDbConnection db = DatabaseConnection.GetConnection();
-
-                string sql = @"
-                    SELECT
-                        IdBitacora,
-                        Usuario AS NombreUsuario,
-                        Accion,
-                        (Tabla + ' | ' + ISNULL(RegistroAfectado, '')) AS Detalles,
-                        Fecha
-                    FROM Seguridad.Bitacora
-                    WHERE (@Texto = '' OR Usuario LIKE '%' + @Texto + '%' OR Accion LIKE '%' + @Texto + '%')
-                    ORDER BY Fecha DESC";
-
-                var result = await db.QueryAsync<BitacoraModel>(
-                    sql,
-                    new { Texto = TextoBusqueda ?? string.Empty });
-
+                var result = await _bitacoraRepository.ListarMovimientosAsync(TextoBusqueda);
                 RegistrosAuditoria = new ObservableCollection<BitacoraModel>(result);
             }
             catch (Exception ex)

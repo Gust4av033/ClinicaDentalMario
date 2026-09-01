99e9231d98c38a5055820ed2a4ab40310d50bad2
Gust4av033 <137726376+Gust4av033@users.noreply.github.com>
2026-08-31 19:29:28 -0600

    feat: add common message type

---
+++ C#/MAIN/ClinicaDentalMario/ViewModel/Base/TipoMensaje.cs
@@ -0,0 +1,11 @@
+namespace ClinicaDentalMario.ViewModel.Base
+{
+    public enum TipoMensaje
+    {
+        Ninguno = 0,
+        Informacion = 1,
+        Exito = 2,
+        Advertencia = 3,
+        Error = 4
+    }
+}

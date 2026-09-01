dc90e4205fcd7387b73d011bf11d2db087ea3017
Gust4av033 <137726376+Gust4av033@users.noreply.github.com>
2026-08-31 20:57:55 -0600

    refactor: inject shell view model into main window

--- C#/MAIN/ClinicaDentalMario/Views/MainWindow.xaml.cs
+++ C#/MAIN/ClinicaDentalMario/Views/MainWindow.xaml.cs
@@ -5,24 +5,40 @@ namespace ClinicaDentalMario.Views
 {
     public partial class MainWindow : Window
     {
+        private readonly MainViewModel _viewModel;
+
         public MainWindow()
+            : this(new MainViewModel())
+        {
+        }
+
+        public MainWindow(MainViewModel viewModel)
         {
             InitializeComponent();
 
-            var viewModel = new MainViewModel();
-            viewModel.CierreSesionSolicitado += OnCierreSesionSolicitado;
-            DataContext = viewModel;
+            _viewModel = viewModel ?? throw new ArgumentNullException(nameof(viewModel));
+            _viewModel.CierreSesionSolicitado += OnCierreSesionSolicitado;
+            DataContext = _viewModel;
+
+            Closed += OnClosed;
         }
 
         private void OnCierreSesionSolicitado(object? sender, EventArgs e)
         {
             if (Application.Current is not App app)
             {
+                Application.Current.Shutdown();
                 return;
             }
 
             app.MostrarLogin();
             Close();
+        }
+
+        private void OnClosed(object? sender, EventArgs e)
+        {
+            _viewModel.CierreSesionSolicitado -= OnCierreSesionSolicitado;
+            Closed -= OnClosed;
         }
     }
 }

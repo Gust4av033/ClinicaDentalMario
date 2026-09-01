61eff45e98dc163211bcf270d5558bed8eddb7c6
Gust4av033 <bryangustabo@gmail.com>
2026-08-31 22:27:10 -0600

    Cambios en paciente y agenda

--- C#/MAIN/ClinicaDentalMario/obj/Debug/net8.0-windows/ClinicaDentalMario.AssemblyInfo.cs
+++ C#/MAIN/ClinicaDentalMario/obj/Debug/net8.0-windows/ClinicaDentalMario.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinicaDentalMario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+328ccfe356dd20605852adc117ef68240ac0b369")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9be03d0c524c970eeb1cb33aff99f6eb4d36dbe4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinicaDentalMario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicaDentalMario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

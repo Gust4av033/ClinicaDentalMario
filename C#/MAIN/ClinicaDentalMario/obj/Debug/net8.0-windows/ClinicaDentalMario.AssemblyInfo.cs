8a1894ed007947cccb92e1291cdf97f07702eba2
Gust4av033 <bryangustabo@gmail.com>
2026-09-02 19:09:57 -0600

    Cambios sincronizados

--- C#/MAIN/ClinicaDentalMario/obj/Debug/net8.0-windows/ClinicaDentalMario.AssemblyInfo.cs
+++ C#/MAIN/ClinicaDentalMario/obj/Debug/net8.0-windows/ClinicaDentalMario.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinicaDentalMario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61eff45e98dc163211bcf270d5558bed8eddb7c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fab2cd62001176e5592217806769500f73b51722")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinicaDentalMario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicaDentalMario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

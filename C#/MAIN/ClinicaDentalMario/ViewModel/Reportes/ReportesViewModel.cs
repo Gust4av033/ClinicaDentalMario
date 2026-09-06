b37f3a2a9f6f75013f3fc1a50176fb18304c0424
Gust4av033 <137726376+Gust4av033@users.noreply.github.com>
2026-09-06 09:55:00 -0600

    feat(reportes): habilitar navegación a reportes funcionales

--- C#/MAIN/ClinicaDentalMario/ViewModel/Reportes/ReportesViewModel.cs
+++ C#/MAIN/ClinicaDentalMario/ViewModel/Reportes/ReportesViewModel.cs
@@ -1,54 +1,64 @@
-﻿using ClinicaDentalMario.ViewModel.Base;
-using ClinicaDentalMario.Views.Reportes; // Necesario para instanciar las vistas
-using System;
+using ClinicaDentalMario.ViewModel.Base;
+using ClinicaDentalMario.Views.Reportes;
 using System.Windows.Input;
 
 namespace ClinicaDentalMario.ViewModel.Reportes
 {
     public class ReportesViewModel : ViewModelBase
     {
-        // 🔥 CAMBIO AQUÍ: Cambiamos Action<ViewModelBase> por Action<object> 🔥
         private readonly Action<object> _navegar;
 
         public ICommand AbrirReporteIngresosCommand { get; }
         public ICommand AbrirReportePacientesCommand { get; }
+        public ICommand AbrirReporteSaldosCommand { get; }
         public ICommand AbrirReporteTratamientosCommand { get; }
         public ICommand AbrirReporteAgendaCommand { get; }
-        public ICommand AbrirReporteMarketingCommand { get; }
-        public ICommand AbrirReporteCatálogoCommand { get; }
 
-        // 🔥 CAMBIO AQUÍ TAMBIÉN: En el parámetro del constructor 🔥
         public ReportesViewModel(Action<object> navegar)
         {
-            _navegar = navegar;
-            Titulo = "Centro de Reportes y Estadísticas";
+            _navegar = navegar ?? throw new ArgumentNullException(nameof(navegar));
+            Titulo = "Centro de Reportes";
 
             AbrirReporteIngresosCommand = new RelayCommand(AbrirIngresos);
-            AbrirReportePacientesCommand = new RelayCommand(p => { /* Próximamente */ });
-            AbrirReporteTratamientosCommand = new RelayCommand(p =>
+            AbrirReportePacientesCommand = new RelayCommand(AbrirPacientes);
+            AbrirReporteSaldosCommand = new RelayCommand(AbrirSaldos);
+            AbrirReporteTratamientosCommand = new RelayCommand(AbrirProductividad);
+            AbrirReporteAgendaCommand = new RelayCommand(AbrirAgenda);
+        }
+
+        private void AbrirIngresos(object? parameter)
+        {
+            NavegarA(new ReporteIngresosView(), new ReporteIngresosViewModel(_navegar));
+        }
+
+        private void AbrirPacientes(object? parameter)
+        {
+            NavegarA(new ReportePacientesView(), new ReportePacientesViewModel(_navegar));
+        }
+
+        private void AbrirSaldos(object? parameter)
+        {
+            NavegarA(new ReporteSaldosView(), new ReporteSaldosViewModel(_navegar));
+        }
+
+        private void AbrirProductividad(object? parameter)
+        {
+            NavegarA(new ReporteProductividadView(), new ReporteProductividadViewModel(_navegar));
+        }
+
+        private void AbrirAgenda(object? parameter)
+        {
+            NavegarA(new ReporteAgendaView(), new ReporteAgendaViewModel(_navegar));
+        }
+
+        private void NavegarA(object vista, object viewModel)
+        {
+            if (vista is System.Windows.FrameworkElement elemento)
             {
-                var vista = new ReporteProductividadView();
-                vista.DataContext = new ReporteProductividadViewModel(_navegar);
-                _navegar(vista);
-            });
-            AbrirReporteAgendaCommand = new RelayCommand(p => { /* Próximamente */ });
-            AbrirReporteMarketingCommand = new RelayCommand(p => { /* Próximamente */ });
-            AbrirReporteCatálogoCommand = new RelayCommand(p => { /* Próximamente */ });
+                elemento.DataContext = viewModel;
+            }
+
+            _navegar(vista);
         }
-
-        private void AbrirIngresos(object? parameter) 
-        { 
-            // 1. Creamos la vista física
-            var vista = new ReporteIngresosView();
-
-            // 2. Creamos su cerebro
-            var viewModel = new ReporteIngresosViewModel(_navegar);
-            // 3. Los conectamos
-            vista.DataContext = viewModel;
-            
-            // 4. Le decimos al MainWindow que pinte esta vista
-            _navegar(vista); 
-        }
-
     }
 }

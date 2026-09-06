f35f5aa02692d435c504622f2b37617a804ac926
Gust4av033 <137726376+Gust4av033@users.noreply.github.com>
2026-09-06 09:54:30 -0600

    feat(reportes): agregar vista de citas y asistencia

---
+++ C#/MAIN/ClinicaDentalMario/Views/Reportes/ReporteAgendaView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace ClinicaDentalMario.Views.Reportes
+{
+    public partial class ReporteAgendaView : UserControl
+    {
+        public ReporteAgendaView()
+        {
+            InitializeComponent();
+        }
+    }
+}

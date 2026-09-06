7204075d87c593dcacf2dbd075fcbac259f489c3
Gust4av033 <bryangustabo@gmail.com>
2026-09-05 20:41:04 -0600

    algunos cambios good

--- C#/MAIN/ClinicaDentalMario/ViewModel/Pacientes/HistorialPacienteViewModel.cs
+++ C#/MAIN/ClinicaDentalMario/ViewModel/Pacientes/HistorialPacienteViewModel.cs
@@ -9,6 +9,7 @@ using ClinicaDentalMario.Views.Pacientes;
 using ClinicaDentalMario.Views.Tratamientos;
 using Microsoft.Win32;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Windows.Input;
 
 namespace ClinicaDentalMario.ViewModel.Pacientes
